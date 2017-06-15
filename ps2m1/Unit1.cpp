6b8d9cadd872b2a0efae468fce5cf324c91a1c82
Voltron2 <voltron.creater@gmail.com>
2017-06-15 22:11:23 +0300

    Add files via upload

--- ps2m1/Unit1.cpp
+++ ps2m1/Unit1.cpp
@@ -12,12 +12,30 @@ TForm1 *Form1;
 __fastcall TForm1::TForm1(TComponent* Owner)
 	: TForm(Owner)
 {
-//hero1[0].name="Rolf Landale";
-hero1[0]=Theroes("Rolf Landale");
+class_h[0] = Tjob("Agent", 15, 0xffffffff, 15, 15, 15, 15, 15, 15,
+		15, 15); //
+	class_h[1] = Tjob("None", 10, 0xffffffff, 10, 10, 10, 10, 10, 10,
+		10, 10);
+	class_h[2] = Tjob("hunter", 20, 0xffffffff, 20, 20, 20, 20, 20, 20,
+		20, 20);
+	class_h[3] = Tjob("Doctor", 30, 0xffffffff, 30, 30, 30, 30, 30, 30,
+		30, 30);
+	class_h[4] = Tjob("Biologist", 40, 0xffffffff, 40, 40, 40, 40, 40, 40,
+		40, 40);
+	class_h[5] = Tjob("Guardian", 50, 0xffffffff, 50, 50, 50, 50, 50, 50,
+		50, 50);
+	class_h[6] = Tjob("Wrecker", 60, 0xffffffff, 60, 60, 60, 60, 60, 60,
+		60, 60);
+	class_h[7] = Tjob("Thief", 70, 0xffffffff, 70, 70, 70, 70, 70, 70,
+		70, 70);
+
+		hero1[0]=Theroes("Rolf Landale",1);
 }
 //---------------------------------------------------------------------------
 void __fastcall TForm1::FormPaint(TObject *Sender)
 {
-Canvas->TextOutW(0,0,hero1[0].name);
+ImageList1->Draw(Canvas, 8*1, 8*1,hero1[0].face , 1);
+Canvas->TextOutW(8*1,8*13,hero1[0].name);
+Canvas->TextOutW(8*1,8*15,class_h[0].name);
 }
 //---------------------------------------------------------------------------
---
+++ ps2m1/__history/Unit1.h.~3~
@@ -0,0 +1,44 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+#include <System.ImageList.hpp>
+#include <Vcl.ImgList.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TImageList *ImageList1;
+	void __fastcall FormPaint(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+
+	//my
+    class Theroes
+{
+private:
+
+public:
+AnsiString name;
+
+Theroes(){};
+Theroes(AnsiString arg1){
+name=arg1;
+
+};
+Theroes_status(){
+
+};
+}hero1[8];
+};
+
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
